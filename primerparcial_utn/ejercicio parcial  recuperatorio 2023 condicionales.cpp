5543378fe927fdf35dbe345a8fb404c7f4c3428d
marugrabelli <marinagrabelli@gmail.com>
2023-11-12 12:14:36 -0300

    Create ejercicio parcial  recuperatorio 2023 condicionales.cpp

---
+++ primerparcial_utn/ejercicio parcial  recuperatorio 2023 condicionales.cpp
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+
+
+int main ()  {
+
+   int j=0;
+
+   for ( int i=20;i >=0; i-=2) {
+
+        j =j+i;
+        if (j <10 && y j %2) {
+            cout <<i<<endl;
+        }
+        if (j >10 && y j %2) {
+            cout <<j<<endl;
+        }
+
+   }
+    return 0;
+}
+
+
+
+
+
